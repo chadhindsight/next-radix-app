f18a767553033fff68feb9cd390717bfbc79a128
chad hinds <chadrickj8@gmail.com>
2023-10-02 11:24:22 -0400

    Add issue details that will be used to populate a list of tickets

---
+++ src/app/utils/userIssues.ts
@@ -0,0 +1,25 @@
+// issueTitle: string,
+//     badgeText: string,
+//         issueDetails: string,
+//             comments: string
+
+export const userIssues = [
+    {
+        issueTitle: "Issue #24 - Button Is Wrong Color",
+        badgeText: "Backend",
+        issueDetails: "Users are consistently experiencing a problem where the button color on the application's interface appears to be incorrect. This issue has been reported multiple times and requires immediate attention.",
+        issueComments: "7 Comments"
+    },
+    {
+        issueTitle: "Issue #26 - Price displayed is incorrect",
+        badgeText: "Fullstack",
+        issueDetails: "Users keep seeing 'lorem ipsum' instead of the actual price of the product.",
+        issueComments: "4 Comments"
+    },
+    {
+        issueTitle: "Issue #25 - User Information Is Doubled Instead Of Single",
+        badgeText: "Frontend",
+        issueDetails: "Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        issueComments: "5 Comments"
+    }
+]
