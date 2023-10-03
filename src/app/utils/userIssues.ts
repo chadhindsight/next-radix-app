7320aadb0eb67ce2d1c0fb3d59e6126f6d5b6270
chad hinds <chadrickj8@gmail.com>
2023-10-03 10:41:07 -0400

    Change issueComments to numbers instead of strings

--- src/app/utils/userIssues.ts
+++ src/app/utils/userIssues.ts
@@ -8,18 +8,18 @@ export const userIssues = [
         issueTitle: "Issue #24 - Button Is Wrong Color",
         badgeText: "Backend",
         issueDetails: "Users are consistently experiencing a problem where the button color on the application's interface appears to be incorrect. This issue has been reported multiple times and requires immediate attention.",
-        issueComments: "7 Comments"
+        issueComments: 7
     },
     {
         issueTitle: "Issue #26 - Price displayed is incorrect",
         badgeText: "Fullstack",
         issueDetails: "Users keep seeing 'lorem ipsum' instead of the actual price of the product.",
-        issueComments: "4 Comments"
+        issueComments: 4
     },
     {
         issueTitle: "Issue #25 - User Information Is Doubled Instead Of Single",
         badgeText: "Frontend",
         issueDetails: "Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        issueComments: "5 Comments"
+        issueComments: 5
     }
 ]
